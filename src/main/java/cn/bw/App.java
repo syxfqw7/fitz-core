b5c42879676ed3df99d8165ca7ca0046746c0161
xuef <553583365@qq.com>
2018-12-03 23:06:33 +0800

    test git push

--- src/main/java/cn/bw/App.java
+++ src/main/java/cn/bw/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Let's start at fitz-core" );
+        System.out.println( "Hello World! Let's start at fitz-core..." );
     }
 }
