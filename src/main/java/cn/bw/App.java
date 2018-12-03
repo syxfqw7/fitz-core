6723d86fd3779130521e340b5820c29c9c123f26
xuef <553583365@qq.com>
2018-12-03 22:52:52 +0800

    初始化

---
+++ src/main/java/cn/bw/App.java
@@ -0,0 +1,13 @@
+package cn.bw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Let's start at fitz-core" );
+    }
+}
