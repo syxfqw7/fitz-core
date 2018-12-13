f059de8b8acb3559d53adbef40e8e1f3fe0ef3bd
fitz <553583365@qq.com>
2018-12-13 16:38:37 +0800

    package jar without resources

--- src/test/java/cn/bw/FormatTest.java
+++ src/test/java/cn/bw/FormatTest.java
@@ -1,56 +1,51 @@
-///**
-// * FileName: FormatTest
-// * Author:   jack.xue
-// * Date:     2018/12/4 14:13
-// * Description: FormatTest
-// * History:
-// * <author>          <time>          <version>          <desc>
-// * jack.xue           2018/12/4           1.0.0              描述
-// */
-//package cn.bw;
-//
-//import org.apache.log4j.helpers.LogLog;
-//import org.junit.Test;
-//
-//import java.io.File;
-//import java.text.ParseException;
-//import java.text.SimpleDateFormat;
-//import java.util.Arrays;
-//import java.util.Date;
-//
-///**
-// * 〈TODO〉<br>
-// *
-// * @author jack.xue
-// * @create 2018/12/4
-// * @since 1.0.0
-// */
-//public class FormatTest {
-//
-//    String datePattern = "'.'yyyy-MM-dd";
-//    SimpleDateFormat sdf = new SimpleDateFormat(datePattern);
-//
-//    @Test
-//    public void FormatDate(){
-//        String fileName = "E:\\app\\shuku\\log\\ht-nlp-analysis";
-//        File file = new File(fileName);
-//        if (file.getParentFile().exists()){
-//            File[] files = file.getParentFile().listFiles(new LogFileFilter(file.getName()));
-//            Long[] dateArray = new Long[files.length];
-//            for (int i = 0; i < files.length; i++) {
-//                File fileItem = files[i];
-//                String fileDateStr = fileItem.getName().replace(file.getName(), "");
-//                Date filedate = null;
-//                try {
-//                    filedate = sdf.parse(fileDateStr);
-//                    long fileDateLong = filedate.getTime();
-//                    dateArray[i] = fileDateLong;
-//                } catch (ParseException e) {
-//                    LogLog.error("Parse File Date Throw Exception : " + e.getMessage());
-//                }
-//            }
-//            Arrays.sort(dateArray);
-//        }
-//    }
-//
-//}
+/**
+ * FileName: FormatTest
+ * Author:   jack.xue
+ * Date:     2018/12/4 14:13
+ * Description: FormatTest
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * jack.xue           2018/12/4           1.0.0              描述
+ */
+package cn.bw;
+
+import cn.bw.core.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 〈TODO〉<br>
+ *
+ * @author jack.xue
+ * @create 2018/12/4
+ * @since 1.0.0
+ */
+@Slf4j
+public class FormatTest {
+
+    String datePattern = "'.'yyyy-MM-dd";
+    SimpleDateFormat sdf = new SimpleDateFormat(datePattern);
+
+    @Test
+    public void FormatDate(){
+        Date filedate = null;
+        try {
+            String fileDateStr = "2018-12-04-18-24";
+            datePattern = "yyyy-MM-dd-HH";
+            sdf = new SimpleDateFormat(datePattern);
+            log.info("util parse : " + DateUtil.toDate(fileDateStr, datePattern));
+            filedate = sdf.parse(fileDateStr);
+            long fileDateLong = filedate.getTime();
+            log.info("fileDateLong :{}",fileDateLong);
+            String resName = sdf.format(fileDateLong);
+            log.info("resName: {}, {}", resName, resName.equals(fileDateStr));
+        } catch (ParseException e) {
+            log.error("Parse File Date Throw Exception : " + e.getMessage());
+        }
+    }
+
+}
